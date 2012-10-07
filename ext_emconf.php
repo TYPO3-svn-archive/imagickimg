27b36f3f449591440b4ebda5b3ee9944310b50f8
tomkraw <tomkraw@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-07 11:20:21 +0000

    * Fixed dependency log messages in TYPO3 v.4.7 - t3lib_div::int_from_ver(). * The ext is now compatibile with TYPO3 v.4.7.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/imagickimg/trunk@66675 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "imagickimg".
 #
-# Auto generated 02-10-2012 21:09
+# Auto generated 07-10-2012 07:33
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Resize FE and BE images with Imagick PHP extension. Use all image effects available in standard CE elements like Image or Text with image. Useful on servers where exec() function is disabled.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '0.1.0',
+	'version' => '0.1.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,14 +34,16 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-4.6.0'
+			'typo3' => '4.5.0-4.7.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:12:{s:51:"class.tx_imagickimg_WarningMessagePostProcessor.php";s:4:"c906";s:25:"class.ux_SC_show_item.php";s:4:"1da6";s:29:"class.ux_t3lib_stdgraphic.php";s:4:"1e9a";s:26:"class.ux_tslib_content.php";s:4:"9c5e";s:29:"class.ux_tslib_gifbuilder.php";s:4:"8f2e";s:21:"ext_conf_template.txt";s:4:"bd75";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"aef7";s:14:"ext_tables.php";s:4:"c1ad";s:10:"thumbs.php";s:4:"60b6";s:22:"lang/locallang_ttc.xlf";s:4:"93b6";s:22:"lang/locallang_ttc.xml";s:4:"f54e";}',
+	'_md5_values_when_last_written' => 'a:12:{s:51:"class.tx_imagickimg_WarningMessagePostProcessor.php";s:4:"c906";s:25:"class.ux_SC_show_item.php";s:4:"1da6";s:29:"class.ux_t3lib_stdgraphic.php";s:4:"7c35";s:26:"class.ux_tslib_content.php";s:4:"9c5e";s:29:"class.ux_tslib_gifbuilder.php";s:4:"d09a";s:21:"ext_conf_template.txt";s:4:"bd75";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"aef7";s:14:"ext_tables.php";s:4:"c1ad";s:10:"thumbs.php";s:4:"60b6";s:22:"lang/locallang_ttc.xlf";s:4:"93b6";s:22:"lang/locallang_ttc.xml";s:4:"f54e";}',
+	'suggests' => array(
+	),
 );
 
 ?>
